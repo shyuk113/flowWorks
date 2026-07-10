4e2d502deb778f4c98730af0d42a4d18b137357e
이성혁 <danial2569@gmail.com>
2026-07-10 23:36:35 +0900

    feat: add Department, Team, Rank, Employee entities

---
+++ src/main/java/com/example/FlowWorks/department/domain/Department.java
@@ -0,0 +1,23 @@
+package com.example.FlowWorks.department.domain;
+
+import com.example.FlowWorks.employee.domain.Employee;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_head_id", unique = true)
+    private Employee departmentHead;
+}
---
+++ src/main/java/com/example/FlowWorks/employee/domain/Employee.java
@@ -0,0 +1,32 @@
+package com.example.FlowWorks.employee.domain;
+
+import com.example.FlowWorks.rank.domain.Rank;
+import com.example.FlowWorks.team.domain.Team;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private EmployeeStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rank_id", nullable = false)
+    private Rank rank;
+
+}
