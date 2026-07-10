05ed201e937433b6eb028e91ec6ea3f3a62fafe4
이성혁 <danial2569@gmail.com>
2026-07-10 13:02:37 +0900

    feat: Initialize project layout and gradle wrapper

---
+++ src/main/java/com/example/FlowWorks/FlowWorksApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlowWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FlowWorks/FlowWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlowWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
